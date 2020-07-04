f6836c70e6aa71069bc11018d212788c05df91fd
JavaEEPRO <j2eengineer@gmail.com>
2020-07-04 08:10:08 +0300

    refactoring (structure fix): pom.xml moved to root; added main class and application.properties

---
+++ src/main/java/si/inspired/votingsys/VotingsysApp.java
@@ -0,0 +1,13 @@
+package si.inspired.votingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingsysApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingsysApp.class, args);
+    }
+
+}
